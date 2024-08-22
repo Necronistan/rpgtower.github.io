56c557d8f317ce6b9d0561a0353c40b20d25e5d2
JeremyNickel <jeremy.nickel@web.de>
2024-08-22 12:20:58 +0200

    Framework Testing

--- lwjgl3/src/main/java/rpgtower/github/io/git/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/rpgtower/github/io/git/lwjgl3/Lwjgl3Launcher.java
@@ -1,5 +1,7 @@
 package rpgtower.github.io.git.lwjgl3;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import rpgtower.github.io.git.Drop;
@@ -8,7 +10,8 @@ public class Lwjgl3Launcher {
     public static void main (String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Drop");
-        config.setWindowedMode(800, 480);
+        config.setWindowedMode(640, 320);
+        config.setMaximized(true);
         config.useVsync(true);
         config.setForegroundFPS(60);
         new Lwjgl3Application(new Drop(), config);
